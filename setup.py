493557a07354276a6ce0bc8e00dd1787b892738e
leobugeja <bugeja.leo@gmail.com>
2020-08-01 17:12:26 +0100

    python module setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="francis",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "xeno-canto"
+    ],
+    entry_points={
+        "console_script":[
+            "francis = francis.__main__:main"
+        ]
+    }
+)
