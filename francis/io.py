8c44e19e3409fcf48d6a60b7d8cb7943c192aeff
chrishop <christopher.hopkins@bbc.co.uk>
2020-08-24 17:37:09 +0100

    some more skeleton files

--- francis/io.py
+++ francis/io.py
@@ -1,2 +1,8 @@
 # input output to files is in its own file 
 # so it makes the rest of the code easier to test without side effects
+
+import librosa
+
+"""returns a tuple audio, sample_rate"""
+def open_audio(filepath):
+    librosa.load(filepath)
