3050a12575d3a489c76b7834d49651a2990b3400
chrishop <christopher.hopkins@bbc.co.uk>
2020-08-25 19:38:39 +0100

    moved implementation to francis file

--- francis/noise_reduction.py
+++ francis/noise_reduction.py
@@ -1,16 +1,50 @@
-import noisereduce as nr
+import numpy as np
+import librosa
+import noisereduce
 
 
-def process(audio_data):
-    data, rate = audio_data
-    backround_noise, rate = __find_backround_noise(data)
-    return nr.reduce_noise(
-        audio_clip=data,
-        noise_clip=backround_noise,
-        verbose=True)
+def process(audio_data, seconds=2):
+    return noisereduce.reduce_noise(
+        audio_clip=audio_data[0],
+        noise_clip=__backround_noise(audio_data, seconds),
+        verbose=False)
 
 
-def __find_backround_noise(audio_data):
-    """finds a 5s of quietest part of the audio"""
+def __backround_noise(audio_data, seconds):
+    return __quietest(
+        __split_audio(
+            audio_data,
+            seconds
+        )
+    )
+
+
+def __quietest(split_amplitudes):
+    quietest_volume = None
+    quietest_index = 0
+    for i in range(len(split_amplitudes)):
+        average_volume = np.mean(np.abs(split_amplitudes[i]))
+        if quietest_volume == None or quietest_volume > average_volume:
+            quitest_volume = average_volume
+            quietest_index = i
+    return split_amplitudes[quietest_index]
     
-    return audio_data
+    
+def __split_audio(audio_data, seconds):
+    amplitudes, rate = audio_data
+    size_of_elements = rate * seconds
+    number_of_elements = len(amplitudes) // size_of_elements
+    block = size_of_elements * number_of_elements
+    divisible_amplitudes = amplitudes[:block]
+    leftover_amplitudes = amplitudes[block:]
+    
+    return __append_leftover(
+       np.split(divisible_amplitudes, number_of_elements),
+       leftover_amplitudes 
+    )
+    
+    
+def __append_leftover(split_amplitudes, leftover):
+    split_amplitudes[-1] = np.append(split_amplitudes[-1], leftover)
+    return split_amplitudes
+    
--- test/noise_reduction_test.py
+++ test/noise_reduction_test.py
@@ -1,56 +1,16 @@
 import unittest
-import numpy as np
-import librosa
 import soundfile as sf
-import noisereduce
+from francis import noise_reduction
 
-
-def split_audio(audio_data, seconds):
-    amplitudes, sample_rate = audio_data
-    chunk_size = sample_rate * seconds
-    chunk_number = len(amplitudes) // chunk_size
-    block = chunk_number * chunk_size 
-    new_amplitudes = amplitudes[:block]
-    leftover_amplitudes = amplitudes[block:]
-    
-    split_audio = np.split(new_amplitudes, chunk_number)
-    split_audio[-1] = np.append(split_audio[-1], leftover_amplitudes)
-    return split_audio
-
-
-def write_to_file(splitted_audio, sample_rate):
-    for i,elem in enumerate(splitted_audio):
-        sf.write(
-            f"test/fixtures/split_birdsong_{i}.wav",
-            elem,
-            sample_rate)
-        
-        
-def select_quietest(split_audio):
-    quietest_volume = None
-    quietest_index = 0
-    for i in range(len(split_audio)):
-        average_volume = np.mean(np.abs(split_audio[i]))
-        if quietest_volume == None or quietest_volume > average_volume:
-            quitest_volume = average_volume
-            quietest_index = i
-    return split_audio[quietest_index]
-
-def reduce_noise(audio_data):
-    splitted_audio = split_audio(audio_data, 2)
-    quietest = select_quietest(splitted_audio)
-    return noisereduce.reduce_noise(
-        audio_clip=audio_data[0], 
-        noise_clip=quietest)
-    
     
 
 class NoiseReductionTest(unittest.TestCase):
     
+    
+    
     def test_audio_splitting(self):
         audio_data = librosa.load("test/fixtures/sparse_birdsong.mp3")
-        reduced = reduce_noise(audio_data)
-        sf.write("test/fixtures/reduced.wav", reduced, audio_data[1])
+        reduced = noise_reduction.process(audio_data, 2)
+        sf.write("test/fixtures/reduced_real.wav", reduced, audio_data[1])
         
-        # sf.write("test/fixtures/quietest.wav", quietest, audio_data[1])
-        # write_to_file(splitted_audio, audio_data[1])
+       
