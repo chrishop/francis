ce8ef46e46e59fa6e31ee6796db2b8c244a4a0b4
leobugeja <bugeja.leo@gmail.com>
2020-09-03 16:22:23 +0100

    high pass filter change back to 1000Mhz

--- francis/high_pass_filter.py
+++ francis/high_pass_filter.py
@@ -4,7 +4,7 @@ import scipy.signal as sg
 
 def process(audio_data):
     noisy_data, sf = audio_data
-    b, a = sg.butter(4, 1500.0 / (sf / 2.0), "high")
+    b, a = sg.butter(4, 1000.0 / (sf / 2.0), "high")
     filtered_data = sg.filtfilt(b, a, noisy_data)
     return (
         filtered_data,
