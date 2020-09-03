cee21fa8125ab9b84cb87fb8fc08d7dd1761a12c
chrishop <christopher.hopkins@bbc.co.uk>
2020-09-03 13:46:29 +0200

    easier than I thought spectrogram implementation

--- francis/spectrogram.py
+++ francis/spectrogram.py
@@ -1,6 +1,16 @@
+import librosa
+
 # this will produce the mfcc image used to train the neural networks
 
 
-def create(audio_data):
-    mfcc_pandas_table = 0
-    return mfcc_pandas_table
+def add_to_df(the_df):
+    spectrogram_column_data = []
+    for i, row in the_df.iterrows():
+        spectrogram_column_data.append(__create_spectrogram(row["audio_buffer"]))
+
+    the_df["spectrogram"] = spectrogram_column_data
+    return the_df
+
+
+def __create_spectrogram(audio_buffer):
+    return librosa.power_to_db(librosa.feature.melspectrogram(audio_buffer, sr=22050))
