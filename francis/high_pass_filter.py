5096a4f6a834617eebcc9963583262cf42f225ec
leobugeja <bugeja.leo@gmail.com>
2020-09-03 16:59:23 +0100

    change high pass filter format try 2

--- francis/high_pass_filter.py
+++ francis/high_pass_filter.py
@@ -6,7 +6,4 @@ def process(audio_data):
     noisy_data, sf = audio_data
     b, a = sg.butter(4, 1000.0 / (sf / 2.0), "high")
     filtered_data = sg.filtfilt(b, a, noisy_data)
-    return (
-        filtered_data,
-        sf,
-    )  # return filtered_data in same tuple format of audio_data
+    return filtered_data
