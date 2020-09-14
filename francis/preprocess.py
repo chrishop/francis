af1ee930bbf67c51a62c390acdfe24081d02f090
chrishop <christopher.hopkins@bbc.co.uk>
2020-09-14 18:43:53 +0200

    surfaces variables so that config has access

--- francis/preprocess.py
+++ francis/preprocess.py
@@ -1,20 +1,18 @@
 # takes a non processed audio file and returns it when it has been
 # normalised (librosa already does this) , had a high pass filter and noise reduction applied.
 # high pass filter should only be applied to blackbird audio files not the others
-from francis import split_filter
 from francis import noise_reduction
 from francis import high_pass_filter
 from progress.bar import Bar
 
 
-def process(df, pre_process=True):
+def process(df, sample_rate, pre_process=True):
     if pre_process:
         bar2 = Bar("noise reducing and high pass filtering", max=len(df))
         for i, row in df.iterrows():
             bar2.next()
-            row["audio_buffer"] = noise_reduction.process((row["audio_buffer"], 22050))
-            row["audio_buffer"] = high_pass_filter.process((row["audio_buffer"], 22050))
-        bar2.finish
+            row["audio_buffer"] = noise_reduction.process((row["audio_buffer"], sample_rate))
+            row["audio_buffer"] = high_pass_filter.process((row["audio_buffer"], sample_rate))
+        bar2.finish()
         print()
-    df = split_filter.call(df)
     return df
